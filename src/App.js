a78ba05e0103cc1169f77f5fdb2725251719adb5
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-11-26 02:57:20 +0800

    added a fotter on the site

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import router from "./Pages/Routes/Routes/Routes";
 
 function App() {
   return (
-    <div className="App">
+    <div className="max-w-[1440px] mx-auto">
       <RouterProvider router={router}/>
     </div>
   );
